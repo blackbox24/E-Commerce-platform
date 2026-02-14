e8028cec1e71919a8f4ac490ef39ef111456d29b
blackbox24 <nicardid@gmail.com>
2026-02-14 11:36:56 +0000

    chore: added health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import type {Request, Response} from "express"
 import cors from "cors";
 import * as dotenv from "dotenv"
 
@@ -11,6 +12,10 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.get("/api/healthy",async(req: Request, resp: Response)=>{
+    resp.status(200).json({message:"Server is healthy and running on PORT "+ PORT})
+})
 
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT: ${PORT}`)
