d02e02ecbfc573010eb99eb62e2faea776ccd422
blackbox24 <nicardid@gmail.com>
2026-02-16 09:55:23 +0000

    feat: added update and delete products endpoints

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,6 @@
 import type {Request, Response } from "express"
 import pool from "../config/db.ts";
+import type { AuthRequest } from "../middleware/jwt.middleware.ts";
 
 
 export const getAllProducts = async(req: Request, resp:Response) => {
@@ -94,5 +95,85 @@ export const addProduct = async(req:Request, resp: Response) => {
             return resp.status(409).json({ message: "A product with this name already exists" });
         }
         return resp.status(500).json({message: "Failed to save product", error:error})
+    }
+}
+
+export const updateProduct = async(req:Request, resp: Response) => {
+    try{
+        const { id } = req.params;
+        const {
+            name, 
+            quantity,
+            price,
+        } = req.body
+
+        if(!name || !quantity || !price){
+            return resp.status(400).json({message: "Name, quantity and price fields are required"})
+        }
+        if(price <= 0 || quantity < 1){
+            return resp.status(400).json({message: "Quantity or price should be more than 1"});
+        }
+
+        const productExists = await pool.query(
+            "SELECT * FROM products WHERE id = $1", [id]
+        )
+
+        if(productExists.rows.length === 0){
+            return resp.status(404).json({message:"Product does not exist"});
+        }
+
+        let photo_url;
+        if( req.file ){
+            // store the file locally
+            photo_url = req.file.path;
+            console.log(req.file.path)
+        }
+        else{
+            photo_url = productExists.rows[0].photo_url;
+        }
+        const {rows} = await pool.query(`
+            UPDATE products 
+            SET name = $1, 
+                price = $2, 
+                quantity = $3, 
+                photo_url = $4 
+            RETURNING *;
+        `,[
+            name, 
+            price,
+            quantity,
+            photo_url
+            
+        ])
+
+        return resp.status(201).json({message: "Success", product: rows[0]})
+    }catch(error){
+        console.log(error);
+        if (error?.code === '23505') {
+            return resp.status(409).json({ message: "A product with this name already exists" });
+        }
+        return resp.status(500).json({message: "Failed to save product", error:error})
+    }
+}
+
+
+export const deleteProduct = async(req: AuthRequest, resp: Response) => {
+    try {
+        const { id } = req.params;
+        // TODO
+        // query a delete command 
+        //  check rowCount; return 404 if equal to 0
+        const query = `
+            DELETE FROM products WHERE id = $1
+        `;
+        const result = await pool.query(query, [id]);
+        if(result.rowCount === 0){
+            return resp.status(404).json({message: "Product cannot be found"})
+        }
+        return resp.status(200).json({message: "Success"})
+    } catch (error) {
+        console.error(error);
+        return resp.status(500).json({message:"Unexpected error occurred",error:error})
+        
     }
 }
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllProducts, getSingleProduct, addProduct } from "../controllers/product.controller.ts";
+import { getAllProducts, getSingleProduct, addProduct, updateProduct, deleteProduct } from "../controllers/product.controller.ts";
 import authMiddleware from "../middleware/jwt.middleware.ts";
 import { checkAdminRole } from "../middleware/permission.middleware.ts";
 
@@ -10,5 +10,7 @@ const router = Router();
 router.get("/",authMiddleware, getAllProducts)
 router.get("/:id", authMiddleware, getSingleProduct)
 router.post("/",authMiddleware, checkAdminRole,addProduct);
+router.patch("/:id", authMiddleware, checkAdminRole, updateProduct);
+router.delete("/:id", authMiddleware, checkAdminRole, deleteProduct);
 
 export default router;
